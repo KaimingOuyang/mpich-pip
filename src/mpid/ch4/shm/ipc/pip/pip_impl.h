033793d1c376f22377597d47c7190704c8f5a4f6
Kaiming Ouyang <kouya001@ucr.edu>
2020-10-13 20:20:51 -0500

    shm/pip: fix pip datatype dup function

--- src/mpid/ch4/shm/ipc/pip/pip_impl.h
+++ src/mpid/ch4/shm/ipc/pip/pip_impl.h
@@ -14,16 +14,20 @@
 
 #include "pip_pre.h"
 
+int MPIR_Typerep_pip_create_dup(MPIR_Datatype * dtp, MPIR_Datatype * newtype)
+{
+    if (dtp->is_committed)
+        MPIR_Dataloop_dup(dtp->typerep.handle, &newtype->typerep.handle);
+
+    newtype->typerep.num_contig_blocks = dtp->typerep.num_contig_blocks;
+
+    return MPI_SUCCESS;
+}
+
 MPL_STATIC_INLINE_PREFIX int MPIR_PIP_Type_dup(MPIR_Datatype * old_dtp, MPI_Datatype * newtype)
 {
     int mpi_errno = MPI_SUCCESS;
-    MPIR_Datatype *new_dtp;
-    MPIR_FUNC_TERSE_STATE_DECL(MPID_STATE_MPI_PIP_TYPE_DUP);
-
-    MPIR_ERRTEST_INITIALIZED_ORDIE();
-
-    MPIR_FUNC_TERSE_ENTER(MPID_STATE_MPI_PIP_TYPE_DUP);
-    MPID_THREAD_CS_ENTER(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
+    MPIR_Datatype *new_dtp = 0;
 
     /* allocate new datatype object and handle */
     new_dtp = (MPIR_Datatype *) MPIR_Handle_obj_alloc(&MPIR_Datatype_mem);
@@ -46,8 +50,6 @@ MPL_STATIC_INLINE_PREFIX int MPIR_PIP_Type_dup(MPIR_Datatype * old_dtp, MPI_Data
     new_dtp->true_ub = old_dtp->true_ub;
     new_dtp->true_lb = old_dtp->true_lb;
     new_dtp->alignsize = old_dtp->alignsize;
-    new_dtp->has_sticky_ub = old_dtp->has_sticky_ub;
-    new_dtp->has_sticky_lb = old_dtp->has_sticky_lb;
     new_dtp->is_committed = old_dtp->is_committed;
 
     new_dtp->attributes = NULL; /* Attributes are copied in the
@@ -58,33 +60,20 @@ MPL_STATIC_INLINE_PREFIX int MPIR_PIP_Type_dup(MPIR_Datatype * old_dtp, MPI_Data
     new_dtp->builtin_element_size = old_dtp->builtin_element_size;
     new_dtp->basic_type = old_dtp->basic_type;
 
-    new_dtp->max_contig_blocks = old_dtp->max_contig_blocks;
-
-    new_dtp->typerep = NULL;
+    new_dtp->typerep.handle = NULL;
     *newtype = new_dtp->handle;
 
-    new_dtp->contents = NULL;
-
     if (old_dtp->is_committed) {
-        MPIR_Assert(old_dtp->typerep != NULL);
-        MPIR_Typerep_dup(old_dtp->typerep, &new_dtp->typerep);
         MPID_Type_commit_hook(new_dtp);
-    } else {
-        mpi_errno = MPI_ERR_OTHER;
-        goto fn_fail;
     }
 
-  fn_exit:
-    MPIR_FUNC_TERSE_EXIT(MPID_STATE_MPI_PIP_TYPE_DUP);
-    MPID_THREAD_CS_EXIT(GLOBAL, MPIR_THREAD_GLOBAL_ALLFUNC_MUTEX);
-    return mpi_errno;
+    mpi_errno = MPIR_Typerep_pip_create_dup(old_dtp, new_dtp);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    MPL_DBG_MSG_D(MPIR_DBG_DATATYPE, VERBOSE, "dup type %x created.", *newtype);
 
   fn_fail:
-    /* --BEGIN ERROR HANDLING-- */
-    *newtype = MPI_DATATYPE_NULL;
-    mpi_errno = MPIR_Err_return_comm(NULL, __func__, mpi_errno);
-    goto fn_exit;
-    /* --END ERROR HANDLING-- */
+    return mpi_errno;
 }
 
 #endif
