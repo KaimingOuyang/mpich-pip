1c53c59c162955606bf0013dffe1757cd729e605
Kaiming Ouyang <kouya001@ucr.edu>
2020-12-29 14:52:19 -0600

    set MPICH_THREAD_LEVEL parameter for thread level
    if MPICH_THREAD_LEVEL is larger than requested thread level,
    just force MPICH using MPICH_THREAD_LEVEL

--- src/mpid/ch4/src/ch4_init.c
+++ src/mpid/ch4/src/ch4_init.c
@@ -491,15 +491,31 @@ int MPID_Init_local(int requested, int *provided)
 
     MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPID_INIT_LOCAL);
 
-    switch (requested) {
-        case MPI_THREAD_SINGLE:
-        case MPI_THREAD_SERIALIZED:
-        case MPI_THREAD_FUNNELED:
-            *provided = requested;
-            break;
-        case MPI_THREAD_MULTIPLE:
-            *provided = MAX_THREAD_MODE;
-            break;
+    // switch (requested) {
+    //     case MPI_THREAD_SINGLE:
+    //     case MPI_THREAD_SERIALIZED:
+    //     case MPI_THREAD_FUNNELED:
+    //         *provided = requested;
+    //         break;
+    //     case MPI_THREAD_MULTIPLE:
+    //         *provided = MAX_THREAD_MODE;
+    //         break;
+    // }
+
+    /* Force provided to preset thread level */
+    if (MPICH_THREAD_LEVEL >= requested)
+        *provided = MPICH_THREAD_LEVEL;
+    else {
+        switch (requested) {
+            case MPI_THREAD_SINGLE:
+            case MPI_THREAD_SERIALIZED:
+            case MPI_THREAD_FUNNELED:
+                *provided = requested;
+                break;
+            case MPI_THREAD_MULTIPLE:
+                *provided = MAX_THREAD_MODE;
+                break;
+        }
     }
 
 #ifdef MPL_USE_DBG_LOGGING
